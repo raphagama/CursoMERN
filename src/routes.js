16de8e81a89eae5d54d6879ca6b02594672b6de3
Raphael Gama <raphaelgama19@gmail.com>
2021-06-02 16:45:56 -0300

    Finalização crud produtos e inclusão de rotas de produtos

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = express.Router();
 
 const Usuario = require('./controllers/usuarios.controller')
+const Produto = require('./controllers/produtos.controller')
 
 routes.get('/',Usuario.index);
 
@@ -13,6 +14,14 @@ routes.get('/api/usuarios.details/:_id',Usuario.details);
 routes.post('/api/usuarios',Usuario.create);
 routes.delete('/api/usuarios/:_id',Usuario.delete);
 routes.put('/api/usuarios', Usuario.update);
+
+//Rotas de Produtos
+routes.get('/api/produtos',Produto.index);
+routes.get('/api/produtos.details/:_id',Produto.details);
+routes.post('/api/produtos',Produto.create);
+routes.delete('/api/produtos/:_id',Produto.delete);
+routes.put('/api/produtos', Produto.update);
+
 
 
 module.exports =routes;
